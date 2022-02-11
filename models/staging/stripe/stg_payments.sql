9e2daf5927fdc8f3e7b33a5326e8e63abb2fbfc5
Kengo Matsumura <kengo.matsumura@outlook.com>
2022-02-12 10:05:40 +1100

    Added stg_payments and created fct_orders model. Modified dim_customers model to calculate lifetime_value from fct_orders

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,11 @@
+with payments as (
+    select
+        id as payment_id,
+        orderid as order_id,
+        created as order_date,
+        paymentmethod as payment_method,
+        status,
+        amount/100 as amount
+    from raw.stripe.payment
+)
+select * from payments
